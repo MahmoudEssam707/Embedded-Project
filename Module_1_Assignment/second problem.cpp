b41b59a49e3924088fe7d30a1ddd26c78ae344cc
ZiadAshraf85 <111589136+ZiadAshraf85@users.noreply.github.com>
2023-07-14 18:09:26 +0300

    Update second problem.cpp

--- Module_1_Assignment/second problem.cpp
+++ Module_1_Assignment/second problem.cpp
@@ -14,3 +14,13 @@ int main() {
     cout << "You entered " << time << " in seconds,so the distance is " << distance << " feet." << endl;
     return 0;
 }
+// FUNCTION freefall(time)
+// return (acceleration * (time * time)) / 2)
+//END FUNCTION
+//DECLARE time as integar
+//DISPLAY "Please enter the time in seconds: "
+//READ time
+//COMPUTE distance=call freefall(time)
+//DISPLAY "You entered ", time, " seconds, so the distance is ", distance, " feet."
+// return 0
+//END FUNCTION
