681a1510c826a299944a7d0a06e0f46f04692fdc
zzyadd <91030552+zzyadd@users.noreply.github.com>
2023-07-15 18:34:10 +0300

    comments on 7 problem

--- Module_1_Assignment/seventh_problem.cpp
+++ Module_1_Assignment/seventh_problem.cpp
@@ -1,10 +1,14 @@
+//import libraries
 #include <iostream>
 #include <cmath>
+// define universal gravitational constant
 #define G 6.673*pow(10,-8)
 using namespace std;
+//global variables
 double m1;
 double m2;
 double d;
+//input function to set variables
 void input(double* m1,double* m2, double* d){
     cout<<"what is mass of object 1 in grams?"<<endl;
     cin>>*m1;
@@ -14,9 +18,11 @@ void input(double* m1,double* m2, double* d){
     cin>>*d;
 
 }
+//Force function to calculator Force
 double Force(double m1 , double m2 , double d){
     return (G*m1*m2)/(d*d);
 }
+// function to display results
 void result(double m1 , double m2 , double d, double f){
     cout<<"Gravity G is "<<G<<" N"<<endl;
     cout<<"mass of object 1 in grams is "<<m1<<" G"<<endl;
@@ -27,6 +33,7 @@ void result(double m1 , double m2 , double d, double f){
 
 
 }
+//main function
 int main() {
     input(&m1,&m2,&d);
     double forcee=Force(m1,m2,d);
