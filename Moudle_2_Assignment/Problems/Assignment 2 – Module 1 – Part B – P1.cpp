a7ef8d2ff41e61f013b44b57c2084a5ce1b979e6
Mahmoud Essam <108689968+MahmoudEssam707@users.noreply.github.com>
2023-07-23 19:12:22 +0300

    after organizing all of them

--- Moudle_2_Assignment/Problems/Assignment 2 – Module 1 – Part B – P1.cpp
+++ Moudle_2_Assignment/Problems/Assignment 2 – Module 1 – Part B – P1.cpp
@@ -1,8 +1,8 @@
 // Your First C++ Program
 #include <iostream>
+#define size 5
 using namespace std;
 int main() {
-    int size=5;
     int sum=0;
     int array [size] = {5, 10, 15, 20, 25};
     for(int number=0; number < size ;number++){
@@ -16,4 +16,13 @@ int main() {
 Write a C++ program that initializes an integer array of numbers with 
 the following values: 5, 10, 15, 20, 25. Then, using a loop to calculate 
 and display the sum of all the numbers in the array
+*/
+/*
+1-declare constant called size with value of 5
+2-declare variable called sum to store the sum of array
+3-declare array with size of declared size in first point and putting from 5 to 25 by increasing 5 as sequentially
+4-Starting for loop with i starting from 0, and smaller than the size of array, and increasing by 1
+5-printing each value in array to make sure it's right in calling
+6-will store summation of all this values in sum variable
+7-printing the result of sum
 */
