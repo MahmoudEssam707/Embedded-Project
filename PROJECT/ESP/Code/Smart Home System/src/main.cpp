c2b4c2088f44e348d5c76d71e7dc5927e96f699e
Mahmoud Essam <108689968+MahmoudEssam707@users.noreply.github.com>
2023-08-24 13:23:56 +0300

    Changes

--- PROJECT/ESP/Code/Smart Home System/src/main.cpp
+++ PROJECT/ESP/Code/Smart Home System/src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_AHTX0.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Initialize LCD with the I2C address
+Adafruit_AHTX0 aht;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("AHT21 and LCD demo!");
+
+  lcd.init();
+  lcd.backlight();
+
+  if (!aht.begin())
+  {
+    Serial.println("Could not find AHT21. Check wiring.");
+    while (1)
+      delay(10);
+  }
+  Serial.println("AHT21 found.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp.temperature);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print(humidity.relative_humidity);
+  lcd.print("% rH");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degrees C");
+  Serial.print("Humidity: ");
+  Serial.print(humidity.relative_humidity);
+  Serial.println("% rH");
+
+  delay(1000);
 }
