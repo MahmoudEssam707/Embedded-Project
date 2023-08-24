2c89e8d52309102c4322472e122cf59d1312dfa2
Mahmoud Essam <108689968+MahmoudEssam707@users.noreply.github.com>
2023-08-24 22:07:40 +0300

    Changes

--- PROJECT/ESP/Code/Smart Home System/src/main.cpp
+++ PROJECT/ESP/Code/Smart Home System/src/main.cpp
@@ -1,49 +1,60 @@
-#include <Wire.h>
+// Needed Libraries
+#include <ESP32Servo.h>
 #include <LiquidCrystal_I2C.h>
-#include <Adafruit_AHTX0.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Initialize LCD with the I2C address
-Adafruit_AHTX0 aht;
+#include <Keypad.h>
+// Servo connection
+#define SERVO 23
+Servo Door;
+// Gas connection
+#define Gas 34
+// Pir motion Connection
+#define PIR_MOTION 4
+// Flame Sensor Connection
+#define Flame_Sensor 35
+// Keypad Connection
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
+byte rowPins[ROWS] = {13, 12, 14, 27}; // Change these pins to match your wiring
+byte colPins[COLS] = {26,25,33}; // Change these pins to match your wiring
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+// LCD Connection
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("AHT21 and LCD demo!");
-
   lcd.init();
   lcd.backlight();
-
-  if (!aht.begin())
-  {
-    Serial.println("Could not find AHT21. Check wiring.");
-    while (1)
-      delay(10);
-  }
-  Serial.println("AHT21 found.");
+  pinMode(PIR_MOTION, INPUT);
+  pinMode(Gas, INPUT);
+  pinMode(Flame_Sensor, INPUT);
+  Door.attach(SERVO);
+  lcd.clear();
+  lcd.print("Hello");
 }
 
 void loop()
 {
-  sensors_event_t humidity, temp;
-  aht.getEvent(&humidity, &temp);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temp.temperature);
-  lcd.print(" C");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity: ");
-  lcd.print(humidity.relative_humidity);
-  lcd.print("% rH");
-
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degrees C");
-  Serial.print("Humidity: ");
-  Serial.print(humidity.relative_humidity);
-  Serial.println("% rH");
-
-  delay(1000);
-}
+  char key = keypad.getKey();
+  if (key != NO_KEY)
+  {
+    // Do something with the key, e.g., display it on the LCD
+    lcd.clear();
+    lcd.print("Key Pressed:");
+    lcd.setCursor(0, 1);
+    lcd.print(key);
+  }
+    int motionValue = digitalRead(PIR_MOTION);
+    int gasValue = analogRead(Gas);
+    int flameValue = analogRead(Flame_Sensor);
+    Serial.println(motionValue);
+    Serial.println(gasValue);
+    Serial.println(flameValue);
+    Door.write(70);
+    delay(500);
+  }
